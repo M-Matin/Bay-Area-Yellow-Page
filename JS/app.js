0da5111e1b55a283f608801c8184725d6fbe4e31
Maha Matin <matin643@gmail.com>
2016-08-01 23:00:22 -0700

    Binding Results Into Infowindows

--- JS/app.js
+++ JS/app.js
@@ -1,8 +1,3 @@
-var map;
-
-// Create a new blank array for all the listing markers.
-var markers = [];
-
 var locations = [{
     title: 'DVC',
     location: {
@@ -40,27 +35,48 @@ var locations = [{
         lng: -122.071645
     }
 }];
-//Accessing to third party API called FourSquare and searching with latitude and longitude dynamicaly
-var fourSquare = "https://api.foursquare.com/v2/venues/search?ll=" +
-locations[1].location.lat + ',' + locations[1].location.lng+"&limit=1"+
-'&client_id=UIINSNNIDSR4AR3G0ONZPXP14JGBLPOUWTTCMJO4YPRMVAET'+
-'&client_secret=2352X0JFKIBOWJFHALP5JID24KZUZUNC5PV0M2NVX22343TT' + '&v=20130815&query='+
-locations[1].title;
 
-<<<<<<< HEAD
-=======
-       // Normally we'd have these in a database instead.
-       var locations = [
-         {title: 'DVC', location: {lat: 37.971213, lng: -122.068966}},
-         {title: 'Starbucks', location: {lat: 37.947979, lng: -122.060796}},
-         {title: 'Down Town', location: {lat: 37.944937,  lng: -122.058814}},
-         {title: 'Gym', location: {lat: 37.949252, lng: -122.061667 }},
-         {title: 'Petco', location: {lat: 37.927421, lng: -122.071645}},
-         {title: 'FoodMax', location: {lat:  37.927421, lng:-122.071645}}
-          ];
-          var largeInfowindow = new google.maps.InfoWindow();
-                  var bounds = new google.maps.LatLngBounds();
->>>>>>> origin/master
+var map;
+
+
+// Create a new blank array for all the listing markers.
+var markers = [];
+var locLat = locations[1].location.lat;
+var locLng = locations[1].location.lng;
+var clientID = "UIINSNNIDSR4AR3G0ONZPXP14JGBLPOUWTTCMJO4YPRMVAET";
+var clientSec = "2352X0JFKIBOWJFHALP5JID24KZUZUNC5PV0M2NVX22343TT" ;
+//Accessing to third party API called FourSquare and searching with latitude and longitude dynamicaly
+//Query String bulding
+var fourSquare = "https://api.foursquare.com/v2/venues/search?ll=" +
+    locLat + ',' + locLng + "&limit=1" +
+    '&client_id='+clientID + '&client_secret='+clientSec + '&v=20130815&query=' +
+    locations[1].title;
+
+
+//Sending Request
+ $.ajax({
+    url: fourSquare,
+    dataType: 'json',
+    type: 'get',
+    cashe: false,
+    success: function(data) {
+        //Parsing response
+        $(data.response.venues).each(function(index,value){
+             results = data.response.venues[0];
+             name = results.name;
+             street = results.location.formattedAddress[0];
+     	     city = results.location.formattedAddress[1];
+      	     phone = results.contact.phone;
+
+
+        })//Json Expression
+    }
+});
+
+
+
+var largeInfowindow = new google.maps.InfoWindow();
+var bounds = new google.maps.LatLngBounds();
 
 function initMap() {
     // Constructor creates a new map - only center and zoom are required.
@@ -100,27 +116,26 @@ function initMap() {
     }
     // Extend the boundaries of the map for each marker
     map.fitBounds(bounds);
-}
+} //end initMap
 
 
 
-    // This function populates the infowindow when the marker is clicked. We'll only allow
-    // one infowindow which will open at the marker that is clicked, and populate based
-    // on that markers position.
-    function populateInfoWindow(marker, infowindow) {
-        // Check to make sure the infowindow is not already opened on this marker.
-        if (infowindow.marker != marker) {
-            infowindow.marker = marker;
-            infowindow.setContent(
-                '<div>' + '<h1>' + marker.title + '</h1>' +
-                // ' <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-                // 'https://en.wikipedia.org/w/index.php?title=Uluru</a> '
-                yelp
-                + '</div>');
-            infowindow.open(map, marker);
-            // Make sure the marker property is cleared if the infowindow is closed.
-            infowindow.addListener('closeclick', function() {
-                infowindow.setMarker(null);
-            });
-        }
+// This function populates the infowindow when the marker is clicked. We'll only allow
+// one infowindow which will open at the marker that is clicked, and populate based
+// on that markers position.
+function populateInfoWindow(marker, infowindow) {
+    // Check to make sure the infowindow is not already opened on this marker.
+    if (infowindow.marker != marker) {
+        infowindow.marker = marker;
+        infowindow.setContent(
+            '<div><h1>' + name + "</h1></div>" +
+        '<div>' + street + "</div>" +
+        '<div>' + city + "</div>" +
+        '<div>'+ phone +"</div>");
+        infowindow.open(map, marker);
+        // Make sure the marker property is cleared if the infowindow is closed.
+        infowindow.addListener('closeclick', function() {
+            infowindow.setMarker(null);
+        });
     }
+} //end populateInfoWindow
